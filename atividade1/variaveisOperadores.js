7f50166cdf7162f0b929dfd8bec95c299d37d146
pljm0 <pedrolourencojm@gmail.com>
2024-03-22 10:13:12 -0300

    add atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,17 @@
+let a = 4;
+let b = 2;
+let c = 5
+let d = 7
+let e = 8
+let f = 4
+let g = 9
+let h = 3
+let soma = a+b;
+let subtracao = c-d;
+let multiplicacao = e*f;
+let divisao = g/h;
+
+console.log(`Resultado da soma entre ${a} e ${b} é: ${soma}`);
+console.log(`Resultado da subtração entre ${c} e ${d} é: ${subtracao}`);
+console.log(`Resultado da multiplicação entre ${e} e ${f} é: ${multiplicacao}`);
+console.log(`Resultado da divisão entre ${g} e ${h} é: ${divisao}`);
